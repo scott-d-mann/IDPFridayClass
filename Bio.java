fd8f2a500d125ff667de2a61e973a696bfee0aad
TonyLatrobe <162272948+TonyLatrobe@users.noreply.github.com>
2024-04-12 09:49:29 +1000

    Update Bio.java
    Tony was here

--- Bio.java
+++ Bio.java
@@ -2,5 +2,6 @@ public class Bio{
     public static void main(String[] args){
         System.out.println("Name: John Doe");
         System.out.println("Age: 25");
+        System.out.println("Tony was here");
     }
 }
